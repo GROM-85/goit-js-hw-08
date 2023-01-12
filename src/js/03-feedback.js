f40f519e4c303c03f807b7baad7a6129a26b167b
Roman Gordiienko <romangordiienko@Romans-MacBook-Pro.local>
2023-01-12 10:56:25 +0100

    init commit

---
+++ src/js/03-feedback.js
@@ -0,0 +1,48 @@
+import  throttle  from "lodash.throttle";
+
+const form = document.querySelector(".feedback-form");
+const FORM_STATE_KEY = "feedback-form-state";
+const {elements:{email,message}} = form;// for update values
+
+
+
+
+
+const saveMessageHandler = (event) =>{
+
+    let retrivedData = localStorage.getItem(FORM_STATE_KEY);
+    retrivedData = JSON.parse(retrivedData) || {};
+    retrivedData[event.target.name] = event.target.value;
+
+    console.log(retrivedData);
+    
+    localStorage.setItem(FORM_STATE_KEY,JSON.stringify(retrivedData));
+   
+}
+
+function updateInput(){
+    try{
+        const serializedInputs = localStorage.getItem(FORM_STATE_KEY);
+        const retrivedData = JSON.parse(serializedInputs);
+        console.log(retrivedData);// return object
+        email.value = retrivedData.email || "";
+        message.value = retrivedData.message || "";
+    }
+    catch(error){
+        console.log("Get error state",error.message);
+    }
+}
+
+function formSubmitHandler(event){
+    event.preventDefault();
+    
+    console.log(JSON.parse(localStorage.getItem(FORM_STATE_KEY)));
+    localStorage.removeItem(FORM_STATE_KEY);
+    event.currentTarget.reset();
+}
+
+
+updateInput()
+form.addEventListener("input",throttle(saveMessageHandler,500)); 
+// to use throttle with eventListener need to use "tartget" NOT currentTarget cause during bubbling target all the time the same!!
+form.addEventListener("submit",formSubmitHandler);
